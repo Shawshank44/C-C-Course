31a2f39a73e95685ad276c075a71a3b8b4af010d
Shawshank44 <shashankkoushik44@gmail.com>
2025-01-14 11:57:54 +0530

    added basic problems

---
+++ Problems in C++/Basic Problems/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+
+using namespace std;
+
+
+int main(){
+
+    //1. write a program to print hello world:
+    // cout<<"Hello World"<<endl;
+
+    //2. Write a program to print integer number entered by user:
+    // int UserInput;
+    // cout<<"Enter the number : ";
+    // cin>>UserInput;
+    // cout<<"Your input is : "<<UserInput;
+
+    //3. Write a program to add the integer entered by the user :
+    // int firstnumber, secondnumber;
+    // cout<<"Enter the first number : ";
+    // cin>>firstnumber;
+    // cout<<"Enter the Second number : ";
+    // cin>>secondnumber; 
+
+    // cout<<"Sum of both first and second number : "<<(firstnumber + secondnumber); 
+
+    //4. Write a program where the user is asked to enter two integers(divisor and dividend) and the quotient and the remainder of their 
+    // division is computed (Both divisor and dividend should be integers).
+    // int divisor, remainder, dividend, quotient;
+    // cout<<"Enter the value of the divisor : ";
+    // cin>>divisor;
+    // cout<<"Enter the value of dividend : ";
+    // cin>>dividend;
+    // quotient = dividend/divisor;
+    // cout<<"The value of quotient is : "<<quotient<<" ";
+    // remainder=dividend%divisor;
+    // cout<<"the Value of remainder is : "<<remainder;
+
+
+
+    return 0;
+}
