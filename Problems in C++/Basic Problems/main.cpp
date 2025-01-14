cd09696d553ba3b30b636d124cfe9002fa9f5bdf
Shawshank44 <shashankkoushik44@gmail.com>
2025-01-14 18:00:55 +0530

    updated basic problems

--- Problems in C++/Basic Problems/main.cpp
+++ Problems in C++/Basic Problems/main.cpp
@@ -35,6 +35,37 @@ int main(){
     // remainder=dividend%divisor;
     // cout<<"the Value of remainder is : "<<remainder;
 
+    // 5. Write a Program to find size of int, float, double and char.
+    // int a = 10;
+    // float b = 15.25;
+    // char c = 'C';
+    // double d = 15.3899;
+    // cout<<"The size of a is : "<<sizeof(a)<<endl;
+    // cout<<"The size of b is : "<<sizeof(b)<<endl;
+    // cout<<"The size of c is : "<<sizeof(c)<<endl;
+    // cout<<"The size of d is : "<<sizeof(d)<<endl;
+
+    // 6. write a program to swap two numbers :
+    // int a = 10;
+    // int b = 5;
+    // int temp;
+    // cout<<"The value of a & b before swaping is "<<a<<" and "<<b<<endl;
+    // temp=a;
+    // a=b;
+    // b=temp;
+    // cout<<"The value of a & b after swaping is "<<a<<" and "<<b<<endl;
+
+    // 7. Write a Program to find ASCII value of a character.
+    // int a = 'a';
+    // cout<<"The ASCII Value of a is : "<<a<<endl;
+
+    // 8. Write a Program to Multiply Two Decimal Numbers entered by User:
+    // float first, second;
+    // cout<<"Enter the first number : ";
+    // cin>>first;
+    // cout<<"Enter the Second number : ";
+    // cin>>second;
+    // cout<<"Sum of both first and second number : "<<(first * second);
 
 
     return 0;
