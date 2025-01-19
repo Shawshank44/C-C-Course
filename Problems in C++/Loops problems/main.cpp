a16bbe12aea77f8ad9389d9bd49eb5d737ec90ca
Shawshank44 <shashankkoushik44@gmail.com>
2025-01-19 13:20:33 +0530

    added loops problems

---
+++ Problems in C++/Loops problems/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream> 
+
+using namespace std;
+
+
+int main(){
+
+    // 1. Write a program to calculate the sum of the first N natural numbers (here value of N is entered by the user).
+    // int num;
+    // int sum = 0;
+    // cout<<"Enter the number : ";
+    // cin>>num;
+
+    // for (int i = 1; i < num; i++)
+    // {
+    //     sum = sum + i;
+    // }
+
+    // cout<<"the sum obtained is "<<sum<<endl;
+
+    // 2. write a program to find the factorial of a given number N (N is entered by the user).
+    // int num;
+    // int fact = 1;
+
+    // cout<<"Enter the number : ";
+    // cin>>num;
+
+    // for (int i = 1; i <= num; i++)
+    // {
+    //     fact = fact*i;
+    // }
+    
+    // cout<<"the factorial is : "<<fact<<endl;
+
+    // 3. write a program to generate a multiplication table of a number (entered by the user) using a for loop.
+    // int num;
+    // cout<<"Enter the number to print the table : ";
+    // cin>>num;
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout<<num<<" X "<<i<<" = "<<(num*i)<<endl;
+    // }
+
+
+    // 4. Write a program to display fibonacci series upto certain number n (n is entered by the user).
+    // int num, first_num, sec_num, next;
+    // first_num = 0;
+    // sec_num = 1;
+    // cout<<"Enter the number : ";
+    // cin>>num;
+    // if (num>=1)
+    // {
+    //     cout<<first_num<<" ";
+    // }
+    // if (num >=2)
+    // {
+    //     cout<<sec_num<<" ";
+    // }
+
+    // for (int i = 3; i <= num; i++)
+    // {
+    //     next = first_num+sec_num;
+    //     cout<<next<<" ";
+    //     first_num=sec_num;
+    //     sec_num=next;
+    // }
+
+    // cout<<" "<<endl;
+    
+    
+    
+
+    
+
+     
+    return 0;
+}
