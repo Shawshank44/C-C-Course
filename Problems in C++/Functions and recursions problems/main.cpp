1bb61379d48c16f63c0534e802c5fc6aebb2ff9a
Shawshank44 <shashankkoushik44@gmail.com>
2025-02-01 16:16:32 +0530

    Updated functions and recursions problems

--- Problems in C++/Functions and recursions problems/main.cpp
+++ Problems in C++/Functions and recursions problems/main.cpp
@@ -62,6 +62,55 @@ int GCD(int a, int b){// 5. question function
     
 }
 
+bool isPalindrome(int num, int originalnumber, int reversednumber){// 6. question function
+    if (num == 0)
+    {
+        return originalnumber == reversednumber;
+    }
+    int digit = num%10;
+    reversednumber = reversednumber*10+digit;
+    return isPalindrome(num/10,originalnumber,reversednumber);
+}
+
+bool checkPalindrome(int num){//6. question function
+    if (num<0)
+    {
+        return false;
+    }
+    return isPalindrome(num,num,0);
+}
+
+int reverseNumber(int num, int reversednum){//7. question function
+    if (num == 0)
+    {
+        return reversednum;
+    }
+
+    int digit = num%10;
+    reversednum = reversednum*10+digit;
+    return reverseNumber(num/10,reversednum);
+    
+}
+
+
+int climbStairs(int n){// 8. question function
+    if (n<=1)
+    {
+        return 1;
+    }
+    return climbStairs(n-1)+climbStairs(n-2);
+}
+
+void TowerOfHanoi(int n, char sources, char auxilary, char destination){// 9. question function
+    if (n==1)
+    {
+        cout<<"Move disk 1 from "<<sources<<" to "<<destination<<endl;
+        return;
+    }
+    TowerOfHanoi(n-1,sources,destination,auxilary);
+    cout<<"Move disk "<<n<<" from "<<sources<<" to "<<destination<<endl;
+    TowerOfHanoi(n-1,auxilary,sources,destination);
+}
 
 
 int main(){
@@ -136,9 +185,39 @@ int main(){
     //     cout<<"GCD of "<<num1<<" and "<<num2<<" is "<<result;
     // }
     
-    
-    
 
+    // 6. create a recursive function to check if a given number is a palindrome
+    // int num;
+    // cout<<"Enter the number : ";
+    // cin>>num;
+
+    // if (checkPalindrome(num))
+    // {
+    //     cout<<num<<" is a palindrome number"<<endl;
+    // }else{
+    //     cout<<num<<" is not a palindrome number"<<endl;
+    // }
+
+    // 7. create a recursive function to reverse a number
+    // int num;
+    // cout<<"enter the number ";
+    // cin>>num;
+    // int reversed = reverseNumber(num,0);
+    // cout<<"Reversed Number obtained is : "<<reversed<<endl;
+    
+    // 8. climb stairs problem
+    // int n;
+    // cout<<"Enter the number of stairs : ";
+    // cin>>n;
+    // int ways = climbStairs(n);
+    // cout<<"Number of ways to climb "<<n<<" stairs is "<<ways <<endl;
+
+
+    // 9. Tower of hanoi
+    // int n;
+    // cout<<"Enter the number of disks : ";
+    // cin>>n;
+    // TowerOfHanoi(n,'A','B','C');
 
      
     return 0;
