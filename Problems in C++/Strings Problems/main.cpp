146b0e663746396fdcdb97fd740cdeb607ca96d1
Shawshank44 <shashankkoushik44@gmail.com>
2025-02-08 12:27:27 +0530

    Added String problems

---
+++ Problems in C++/Strings Problems/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream> 
+#include <string>
+#include <cctype> // for isalpha and isdigits
+using namespace std;
+
+
+int main(){
+
+    // 1. Write a program to find the length of a string entered by the user
+    // string inputstring;
+    // cout<<"Enter the string : ";
+    // cin>>inputstring;
+    // int length = inputstring.length();
+    // cout<<"Length of the entered string is "<<length<<endl;
+
+    // 2. Write a program to concatenate (join) two strings entered by user.
+    // string str1,str2;
+    // cout<<"Enter the first string : ";
+    // cin>>str1;
+    // cout<<"Enter the second string : ";
+    // cin>>str2;
+    // cout<<"Concatenation of two strings is : "<<str1<<" "<<str2<<endl;
+
+    // 3. Write a program to find the frequency(repeated characters) of the given character by user in a string.
+    // char inputString[100];
+    // char target;
+    // int frequency = 0;
+    // cout<<"Enter a string : ";
+    // cin>>inputString;
+    // cout<<"Enter the target character : "<<endl;
+    // cin>>target;
+
+    // for (int i = 0; inputString[i] != '\0'; i++)
+    // {
+    //     if (inputString[i] == target)
+    //     {
+    //         frequency++;
+    //     }
+        
+    // }
+
+    // cout<<"Frequency of the "<<target<< " is "<<frequency<<endl;
+
+
+    // 4. Write a program to find the number of vowels, consonants and digits in strings.
+    // char inputstring[100];
+    // cout << "Enter a string: " << endl;
+    // cin >> inputstring;
+
+    // int vowel = 0, consonants = 0, digits = 0;
+
+    // for (int i = 0; inputstring[i] != '\0'; i++) {
+    //     char c = tolower(inputstring[i]);
+    //     if (isalpha(c)) {
+    //         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+    //             vowel++;
+    //         } else {
+    //             consonants++;
+    //         }
+    //     } else if (isdigit(inputstring[i])) {
+    //         digits++;
+    //     }
+    // }
+
+    // cout << "Vowels: " << vowel << endl;
+    // cout << "Consonants: " << consonants << endl;
+    // cout << "Digits: " << digits << endl;
+    
+    // 5. Write a program to remove all characters in a string except alphabets 
+    // char inputString[100];
+    // cout<<"Enter a string : ";
+    // cin>>inputString;
+    // char resultsstring[100];
+    // int resultindex = 0;
+    // for (int i = 0; inputString[i] != '\0'; i++)
+    // {
+    //     if (isalpha(inputString[i]))
+    //     {
+    //         resultsstring[resultindex++] = inputString[i];
+    //     }
+        
+    // }
+    
+    // resultsstring[resultindex] = '\0';
+    // cout<<"Output string obtained is "<<resultsstring<<endl;
+    
+
+     
+    return 0;
+}
