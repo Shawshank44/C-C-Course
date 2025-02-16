a71db892cbfadeb95408a3947085d684da3e0171
Shawshank44 <shashankkoushik44@gmail.com>
2025-02-16 15:23:43 +0530

    Updated Pointers porblems with (intermediate and advance)

--- Problems in C++/Pointers problems/main.cpp
+++ Problems in C++/Pointers problems/main.cpp
@@ -1,6 +1,97 @@
-#include <iostream> 
+#include <iostream>
+#include <memory>
 
 using namespace std;
+
+int add(int a, int b) {// 5. question function
+    return a + b;
+}
+
+int subtract(int a, int b) {// 5. question function
+    return a - b;
+}
+
+int multiply(int a, int b) {// 5. question function
+    return a * b;
+}
+
+void operate(int x, int y, int (*funcPtr)(int, int)) {// 5. question function
+    cout << "Result: " << funcPtr(x, y) << endl;
+}
+
+void printArray(int *arr, int size) {// 6. question function
+    for (int i = 0; i < size; i++) {
+        cout << *(arr + i) << " ";  
+    }
+    cout << endl;
+}
+
+void reverseArray(int *arr, int size) { // 8. question function
+    int *left = arr, *right = arr + size - 1;
+    while (left < right) {
+        swap(*left, *right);
+        left++;
+        right--;
+    }
+}
+
+struct Student {// 9. Question struct
+    string name;
+    int age;
+};
+
+
+struct Node {//10. Question struct
+    int data;
+    Node* next;
+};
+
+void insert(Node*& head, int value) {//10. Question function
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+
+void deleteNode(Node*& head, int value) {//10. Question function
+    Node* temp = head, *prev = nullptr;
+    
+    if (temp != nullptr && temp->data == value) {
+        head = temp->next;
+        delete temp;
+        return;
+    }
+
+    while (temp != nullptr && temp->data != value) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == nullptr) return;
+
+    prev->next = temp->next;
+    delete temp;
+}
+
+void display(Node* head) {//10. Question function
+    while (head) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+class Identity { //11. Question class
+    public:
+        string name;
+        Identity(string n) : name(n) {
+            cout << name << " created.\n";
+        }
+        ~Identity() {
+            cout << name << " destroyed.\n";
+        }
+};
+
 
 
 int main(){
@@ -67,7 +158,7 @@ int main(){
     // }
 
 
-    // Dynamic memory allocation :
+    // 4.Dynamic memory allocation :
     // int n;
     // cout << "Enter the number of elements: ";
     // cin >> n;
@@ -98,6 +189,118 @@ int main(){
 
     // // Free the allocated memory
     // delete[] arr;
+
+
+    // Intermediate Problems :
+    // 5.Pointer to Function
+    // int a = 10, b = 5;
+    // operate(a, b, add);
+    // operate(a, b, subtract);
+    // operate(a, b, multiply);
+
+    // 6.Array pointer :
+    // int arr[] = {10, 20, 30, 40, 50};
+    // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // printArray(arr, size);
+
+    // 7. Pointer to Pointer :
+    // int num = 100;
+    // int *ptr1 = &num;
+    // int **ptr2 = &ptr1;
+
+    // cout << "Value of num: " << num << endl;
+    // cout << "Value using ptr1: " << *ptr1 << endl;
+    // cout << "Value using ptr2: " << **ptr2 << endl;
+    
+    // cout << "Address of num: " << &num << endl;
+    // cout << "Address stored in ptr1: " << ptr1 << endl;
+    // cout << "Address stored in ptr2: " << ptr2 << endl;
+
+    // 8. Reversing of the array :
+    // int arr[] = {1, 2, 3, 4, 5};
+    // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // cout << "Original array: ";
+    // for (int i = 0; i < size; i++) {
+    //     cout << arr[i] << " ";
+    // }
+    // cout << endl;
+
+    // reverseArray(arr, size);
+
+    // cout << "Reversed array: ";
+    // for (int i = 0; i < size; i++) {
+    //     cout << arr[i] << " ";
+    // }
+    // cout << endl;
+
+    // Advance level :
+    // 9. Pointer Structure :
+    // Student *stuPtr = new Student;  
+
+    // cout << "Enter student name: ";
+    // cin >> stuPtr->name;
+    // cout << "Enter student age: ";
+    // cin >> stuPtr->age;
+
+    // cout << "\nStudent Details:" << endl;
+    // cout << "Name: " << stuPtr->name << endl;
+    // cout << "Age: " << stuPtr->age << endl;
+
+    // delete stuPtr;  
+
+
+    //10.Singly Linked List Using Pointers :
+    // Node* head = nullptr;
+
+    // insert(head, 10);
+    // insert(head, 20);
+    // insert(head, 30);
+
+    // cout << "Linked List: ";
+    // display(head);
+
+    // deleteNode(head, 20);
+    // cout << "After deletion: ";
+    // display(head);
+
+    // 11. Smart PointersL:
+    // unique_ptr<Identity> stu1 = make_unique<Identity>("Alice");
+    
+    // shared_ptr<Identity> stu2 = make_shared<Identity>("Bob");
+    // shared_ptr<Identity> stu3 = stu2;  
+
+    // cout << "Shared count: " << stu2.use_count() << endl;
+
+    // 12. 2D Dynamic Array Using Pointers
+    // int rows = 3, cols = 3;
+    // int **matrix = new int*[rows];
+
+    // for (int i = 0; i < rows; i++) {
+    //     matrix[i] = new int[cols];
+    // }
+
+    // cout << "Enter matrix elements:\n";
+    // for (int i = 0; i < rows; i++) {
+    //     for (int j = 0; j < cols; j++) {
+    //         cin >> matrix[i][j];
+    //     }
+    // }
+
+    // cout << "\nMatrix:\n";
+    // for (int i = 0; i < rows; i++) {
+    //     for (int j = 0; j < cols; j++) {
+    //         cout << matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < rows; i++) {
+    //     delete[] matrix[i];
+    // }
+    // delete[] matrix;
+
     
     return 0;
 }
